d2a19005da9e85e643095909801a38c62ac3bf96
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-09-09 12:39:02 -0500

    polyfill

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ const imports = {
         fd_fdstat_set_rights() { return 0; },
         fd_filestat_get() { return 0; },
         fd_filestat_set_size() { return 0; },
+        fd_filestat_set_times() { return 0; },
     }
 };
 fetch('csgp4.wasm')
