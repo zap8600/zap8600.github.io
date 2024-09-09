9d0532264371b0091d2cfd4386e7b5f6362f9c98
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-09-09 12:26:50 -0500

    polyfill

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ const imports = {
         clock_time_get() { return 0; },
         fd_advise() { return 0; },
         fd_allocate() { return 0; },
+        fd_datasync() { return 0; },
     }
 };
 fetch('csgp4.wasm')
